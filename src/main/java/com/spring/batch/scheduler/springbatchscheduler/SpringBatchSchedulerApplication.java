53b6fe4b909e483fc3d664cafabd84c585262865
Kapilan <sriranjankapilan@gmail.com>
2025-11-05 06:56:54 +0530

    initial commit

---
+++ src/main/java/com/spring/batch/scheduler/springbatchscheduler/SpringBatchSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.batch.scheduler.springbatchscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/batch/scheduler/springbatchscheduler/SpringBatchSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.batch.scheduler.springbatchscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
