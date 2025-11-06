e2a470b496f42403989adf888694327c127e4468
Kapilan <sriranjankapilan@gmail.com>
2025-11-06 07:35:51 +0530

    @EnableScheduling added to main class

--- src/main/java/com/spring/batch/scheduler/springbatchscheduler/SpringBatchSchedulerApplication.java
+++ src/main/java/com/spring/batch/scheduler/springbatchscheduler/SpringBatchSchedulerApplication.java
@@ -2,8 +2,10 @@ package com.spring.batch.scheduler.springbatchscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling   // scheduler enabled to this application to execute task for every certain time
 public class SpringBatchSchedulerApplication {
 
     public static void main(String[] args) {
